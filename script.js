0a89af678d6197ee02d71b892d06a498300cb74a
MarkHildegard <mark.krause09@hotmail.com>
2025-03-15 10:35:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const downloadButton = document.getElementById("download-button");
-
-    downloadButton.addEventListener("click", function () {
-        alert("Download will be available soon!");
-    });
-});
