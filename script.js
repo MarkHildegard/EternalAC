9872638f6177b4ffef4b383a699ab0bad18f7b6a
MarkHildegard <mark.krause09@hotmail.com>
2025-03-15 10:36:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const downloadButton = document.getElementById("download-button");
+
+    downloadButton.addEventListener("click", function () {
+        alert("Download will be available soon!");
+    });
+});
